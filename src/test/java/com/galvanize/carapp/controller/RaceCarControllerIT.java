db6f83ee8946e49a9943d660fda21c799f238e3b
Richard Key <rich@busyrich.com>
2021-02-12 13:52:13 -0700

    Set up the tests to use an external postgres DB to debug the Postgres error.

--- src/test/java/com/galvanize/carapp/controller/RaceCarControllerIT.java
+++ src/test/java/com/galvanize/carapp/controller/RaceCarControllerIT.java
@@ -24,7 +24,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@AutoConfigureTestDatabase
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class RaceCarControllerIT {
 
